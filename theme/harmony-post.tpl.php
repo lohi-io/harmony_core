3cc81b82f119b9e0b2d98c50fe7facd2276f1fad
Alli Price <alli.price@yahoo.co.uk>
2014-01-15 20:48:50 +0000

    Improved default post template.

--- theme/harmony-post.tpl.php
+++ theme/harmony-post.tpl.php
@@ -1,3 +1,11 @@
 <?php
 
-print render($content);
+/**
+ * @file
+ * Default theme implementation to display a Harmony Post entity.
+ */
+
+?>
+<div id="post-<?php print $post->post_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print render($content); ?>
+</div>
