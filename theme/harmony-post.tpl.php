17855e7c3d5c4744d3d0641ac428ec487f6cfe9e
Alli Price <alli.price@yahoo.co.uk>
2014-01-16 17:54:17 +0000

    Addition of minimal as possible styling for the post entity.

--- theme/harmony-post.tpl.php
+++ theme/harmony-post.tpl.php
@@ -7,5 +7,16 @@
 
 ?>
 <div id="post-<?php print $post->post_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($content); ?>
+  <a name="post-<?php print $post->post_id; ?>"></a>
+  <div class="post-header text-right text-small">
+    <span class="post-created"><?php print $created; ?></span>
+  </div>
+  <div class="clearfix">
+    <div class="post-left post-user-profile text-center">
+      <?php print $user_picture; ?>
+    </div>
+    <div class="post-content">
+      <?php print render($content); ?>
+    </div>
+  </div>
 </div>
