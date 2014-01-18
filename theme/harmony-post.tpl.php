2242c4fb07f02184c23a6a02bc9199b945148361
Alli Price <alli.price@yahoo.co.uk>
2014-01-18 22:01:43 +0000

    Refactored post template header, added revisions link and "post is reply to..." link.

--- theme/harmony-post.tpl.php
+++ theme/harmony-post.tpl.php
@@ -9,7 +9,19 @@
 <div id="post-<?php print $post->post_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <a name="post-<?php print $post->post_id; ?>"></a>
   <div class="post-header post-left-offset clearfix text-small">
-    <span class="post-created pull-right"><?php print $created; ?></span>
+    <?php if ($direct_reply): ?>
+    <div class="pull-left">
+      <span class="post-is-reply-to">
+        <?php print $direct_reply; ?>
+      </span>
+    </div>
+    <?php endif; ?>
+    <div class="pull-right">
+      <span class="post-created"><?php print $created; ?></span>
+      <?php if ($revisions): ?>
+       <span class="post-revisions"><?php print $revisions; ?></span>
+      <?php endif; ?>
+    </div>
   </div>
   <div class="clearfix">
     <div class="post-left post-user-profile text-center">
