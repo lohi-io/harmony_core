4b5fd6fb5780b1a1f043f5b69327b49f0f09428e
Alli Price <alli.price@yahoo.co.uk>
2014-01-18 21:09:05 +0000

    Refactored the post user profile chunk, now uses user_view and a custom view mode for the user entity. Much better!

--- theme/harmony-post.tpl.php
+++ theme/harmony-post.tpl.php
@@ -8,15 +8,14 @@
 ?>
 <div id="post-<?php print $post->post_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <a name="post-<?php print $post->post_id; ?>"></a>
-  <div class="post-header text-right text-small">
-    <span class="post-created"><?php print $created; ?></span>
+  <div class="post-header post-left-offset clearfix text-small">
+    <span class="post-created pull-right"><?php print $created; ?></span>
   </div>
   <div class="clearfix">
     <div class="post-left post-user-profile text-center">
-      <?php print $user_picture; ?>
-      <?php print $username; ?>
+      <?php print $user_profile; ?>
     </div>
-    <div class="post-content">
+    <div class="post-content post-left-offset">
       <?php print render($content); ?>
     </div>
   </div>
